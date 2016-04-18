ed05181c5383438103960ca6c7b6061efa669abb
hezhenghao <zhenghao.he@gmail.com>
2016-04-18 09:12:58 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,49 +1,50 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function calcASDASCRP() {
+	var result = (0.12 * document.getElementById('bp').value) +
+				 (0.06 * document.getElementById('dms').value) +
+				 (0.11 * document.getElementById('pg').value) +
+				 (0.07 * document.getElementById('pps').value) +
+				 (0.58 * Math.log(1 + (document.getElementById('measurement').value * 1)));
+	return result;
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function calcASDASESR() {
+	var result = (0.08 * document.getElementById('bp').value) +
+				 (0.07 * document.getElementById('dms').value) +
+				 (0.11 * document.getElementById('pg').value) +
+				 (0.09 * document.getElementById('pps').value) +
+				 (0.29 * Math.sqrt(document.getElementById('measurement').value));
+	return result;
+}
 
-        console.log('Received Event: ' + id);
-    }
-};
+function calcASDAS() {
+	var result;
+	if (document.getElementById('crp-or-esr').value == 'crp') {
+		result = calcASDASCRP();
+	} else {
+		result = calcASDASESR();
+	}
+	return result
+}
+
+function onAsdasFormSubmit() {
+	document.getElementById('asdas-form').setAttribute('style', 'display:none');
+	document.getElementById('asdas-result-panel').setAttribute('style', 'display:block');
+	var ASDAS = calcASDAS();
+	document.getElementById('asdas-result').innerHTML = ASDAS.toFixed(1);
+	var backdrop_color;
+	if (ASDAS < 1.3) {
+		backdrop_color = '#ffff33';
+	} else if (ASDAS < 2.1) {
+		backdrop_color = '#ffaa33';
+	} else if (ASDAS < 3.5) {
+		backdrop_color = '#ff5533';
+	} else {
+		backdrop_color = '#ff0033';
+	}
+	document.getElementById('result-backdrop').setAttribute('style', 'background-color: ' + backdrop_color);
+}
+
+function onAsdasResultPanelSubmit() {
+	document.getElementById('asdas-form').setAttribute('style', 'display:block');
+	document.getElementById('asdas-result-panel').setAttribute('style', 'display:none');
+}
